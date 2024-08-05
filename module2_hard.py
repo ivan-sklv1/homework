e6a15e27299f775f59d884b777c211ab39fd2894
Иван <132178381+ivan-sklv1@users.noreply.github.com>
2024-08-05 21:42:56 +0300

    Задание по модулю: "Основные операторы"

---
+++ module2_hard.py
@@ -0,0 +1,13 @@
+import random
+
+n = random.randint(3, 21)
+# n = int(input('Enter a number: '))
+result = []
+
+for i in range(1, n):
+    for j in range(i, n):
+        if i == j:
+            continue
+        if n % (i + j) == 0:
+            result.append(str(i) + str(j))
+print(n, '-', *result, sep='')
