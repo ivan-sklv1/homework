c6ad3f7b01ebf247ddc4dafd5733867465465ffb
Иван <132178381+ivan-sklv1@users.noreply.github.com>
2024-08-25 17:54:40 +0300

    Домашняя работа "Специальные методы классов"

---
+++ module5/module_5_2.py
@@ -0,0 +1,27 @@
+class House:
+    def __init__(self, name, number_on_floors):
+        self.name = name
+        self.number_on_floors = number_on_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_on_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print('Такого этажа не существует.')
+            
+    def __len__(self) -> str:
+        return self.number_on_floors
+    
+    def __str__(self) -> str:
+        return f'Название: {self.name}, количество этажей: {self.number_on_floors}'
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+print(h1)
+print(h2)
+
+print(len(h1))
+print(len(h2))
