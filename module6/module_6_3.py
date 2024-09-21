53f7e8d6b144ca732136168a068311c53d3eb4cc
Иван <132178381+ivan-sklv1@users.noreply.github.com>
2024-09-21 09:19:55 +0300

    Множественное наследование

--- module6/module_6_3.py
+++ module6/module_6_3.py
@@ -1,20 +1,29 @@
 class Horse:
-    x_distance = 0
-    sound = 'Frrr'
+
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
     
     def run(self, dx):
         self.x_distance += dx
-        
+
 
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep and repeat'
+
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep and repeat'
     
     def fly(self, dy):
         self.y_distance += dy
 
 
 class Pegasus(Horse, Eagle):
+
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
+
     
     def move(self, dx, dy):
         self.run(dx)
@@ -24,7 +33,7 @@ class Pegasus(Horse, Eagle):
         return self.x_distance, self.y_distance
     
     def voice(self):
-        print(Eagle.sound)
+        print(self.sound)
 
 p1 = Pegasus()
 
